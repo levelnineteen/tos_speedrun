a4d128e850a5776638a736bb01a9312d201af49c
levelnineteen <levelnineteen@gmail.com>
2016-11-26 23:58:07 +0900

    v1.2.0

--- source/speedrun.lua
+++ source/speedrun.lua
@@ -10,12 +10,14 @@ function SPEEDRUN_ON_INIT(addon, frame)
 	local acutil = require("acutil");
 
 	if not g.loaded then
-		CHAT_SYSTEM(frame:GetName() .. " v1.1.5 loaded!");
+		CHAT_SYSTEM(frame:GetName() .. " v1.2.0 loaded!");
 		g.BeforeTime = os.clock();
 		g.BeforeMoney = _G.GET_TOTAL_MONEY();
 		g.BeforeExp = _G.session.GetEXP();
 		g.CurrentExp = _G.session.GetEXP();
 		g.BeforeMapName = _G.session.GetMapName();
+		g.BeforeCharName = _G.GetMyName();
+		g.FilePath = path.GetDataPath() .. "../addons/speedrun/record.csv";
 		g.isEnable = true;
 		g.FirstCheck = false;
 		g.loaded = true;
@@ -61,10 +63,19 @@ function SPEEDRUN_UPDATE(frame, msg, argStr, argNum)
 		end
 		local e1 = g.CurrentExp - g.BeforeExp;
 		local e2 = g.CurrentJobExp - g.BeforeJobExp;
-		CHAT_SYSTEM("ClearTime:" .. t5 .. "min" .. t4 .. "sec Silver:" .. g3 .. GetCommaedText(g2) .. " Bexp:+" .. GetCommaedText(e1) .. " Jexp:+" .. GetCommaedText(e2));
+		--前回キャラ名と今回キャラ名が違う場合、何も獲得していない場合、表示しない。
+		if g.BeforeCharName == _G.GetMyName() then
+			if g2+e1+e2 ~= 0 then
+				CHAT_SYSTEM("ClearTime:" .. t5 .. "min" .. t4 .. "sec Silver:" .. g3 .. GetCommaedText(g2) .. " Bexp:+" .. GetCommaedText(e1) .. " Jexp:+" .. GetCommaedText(e2));
+				g.OUTPUT(g.BeforeCharName, g.BeforeMapName , t5 ,t4,g2,e1,e2);
+			end
+		else
+			g.BeforeCharName = _G.GetMyName();
+		end
 		g.BeforeTime = t2;
 		g.BeforeMoney = g1;
 		g.BeforeMapID = t1;
+		g.BeforeMapName = _G.session.GetMapName();
 		g.BeforeExp = g.CurrentExp;
 		g.BeforeJobExp = g.CurrentJobExp;
 	end
@@ -89,10 +100,18 @@ function SPEEDRUN_COMMAND()
 	local e1 = g.CurrentExp - g.BeforeExp;
 	local e2 = g.CurrentJobExp - g.BeforeJobExp;
 	CHAT_SYSTEM("ClearTime:" .. t5 .. "min" .. t4 .. "sec Silver:" .. g3 .. GetCommaedText(g2) .. " Bexp:+" .. GetCommaedText(e1) .. " Jexp:+" .. GetCommaedText(e2));
+	g.OUTPUT(g.BeforeCharName, _G.session.GetMapName() , t5 ,t4,g2,e1,e2);
 	g.BeforeTime = t2;
 	g.BeforeMoney = g1;
 	g.BeforeExp = g.CurrentExp;
 	g.BeforeJobExp = g.CurrentJobExp;
 	--バラックに戻る
 	_G.RUN_GAMEEXIT_TIMER("Barrack");
+end
+
+function ADDONS.LV19.SPEEDRUN.OUTPUT(name,map,min,sec,silver,bexp,jexp)
+	local g = _G['ADDONS'][devuser][addonname];
+	local f = io.open(g.FilePath, "a");
+	f:write(name .. "," .. map .. "," .. min .. ":" .. sec .. "," .. silver .. "," .. bexp .. "," .. jexp .. "\n");
+	f:close();
 end
