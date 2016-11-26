488018873645095b1b65c3c372828e5983125124
levelnineteen <levelnineteen@gmail.com>
2016-11-27 07:46:57 +0900

    v1.2.4

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ TreeofSaviorJP\data の中にipfファイルを設置する。
 コマンドを使用した場合、バラックへ戻る。  
 TreeofSaviorJP\addons\speedrun\record.csv フォルダと空のファイルを作って設置すると、記録が書き込まれる。
 ## update history
+### v1.2.4
+レベルアップ後に現在経験が0に戻るのが分かったので、その対応  
 ### v1.2.2
 データが取得できなかった場合にループすることがあるのに対応  
 ### v1.2.1
--- source/speedrun.lua
+++ source/speedrun.lua
@@ -10,11 +10,15 @@ function SPEEDRUN_ON_INIT(addon, frame)
 	local acutil = require("acutil");
 
 	if not g.loaded then
-		CHAT_SYSTEM(frame:GetName() .. " v1.2.2 loaded!");
+		CHAT_SYSTEM(frame:GetName() .. " v1.2.4 loaded!");
 		g.BeforeTime = os.clock();
 		g.BeforeMoney = _G.GET_TOTAL_MONEY();
 		g.BeforeExp = _G.session.GetEXP();
 		g.CurrentExp = _G.session.GetEXP();
+		g.BeforeMaxExp = _G.session.GetMaxEXP();
+		g.LevelupExp = g.BeforeMaxExp - g.CurrentExp;
+		g.TotalExp = 0;
+		g.TotalJobExp = 0;
 		g.BeforeMapName = _G.session.GetMapName();
 		g.BeforeCharName = _G.GetMyName();
 		g.FilePath = path.GetDataPath() .. "../addons/speedrun/record.csv";
@@ -31,17 +35,30 @@ end
 function SPEEDRUN_EXP_UPDATE(frame, msg, argStr, argNum)
 	local g = _G['ADDONS'][devuser][addonname];
 	if msg == 'EXP_UPDATE' then
+		if g.BeforeMaxExp ~= _G.session.GetMaxEXP() then
+			--レベルアップ後はMaxExpが変化している。TotalExpにレベルアップ分を足す必要がある。
+			g.TotalExp = g.TotalExp + g.LevelupExp;
+			g.LevelupExp = _G.session.GetMaxEXP();
+			g.BeforeMaxExp = _G.session.GetMaxEXP();
+		end
 		g.CurrentExp = _G.session.GetEXP();
 	end
 end
 
 function SPEEDRUN_JOB_EXP_UPDATE(frame, msg, str, exp, tableinfo)
 	local g = _G['ADDONS'][devuser][addonname];
+	g.CurrentJobExp = exp;
 	if g.FirstCheck == false then
 		g.BeforeJobExp = exp;
+		g.BeforeMaxJobExp = tableinfo.endExp;
+		g.LevelupJobExp = tableinfo.endExp - g.CurrentJobExp;
 		g.FirstCheck = true;
 	end
-	g.CurrentJobExp = exp;
+	if g.BeforeMaxJobExp ~= tableinfo.endExp then
+		g.TotalJobExp = g.TotalJobExp + g.LevelupJobExp;
+		g.BeforeMaxJobExp = tableinfo.endExp;
+		g.LevelupJobExp = tableinfo.endExp;
+	end
 end
 
 function SPEEDRUN_UPDATE(frame, msg, argStr, argNum)
@@ -61,8 +78,8 @@ function SPEEDRUN_UPDATE(frame, msg, argStr, argNum)
 		else
 			g3 = "";
 		end
-		local e1 = g.CurrentExp - g.BeforeExp;
-		local e2 = g.CurrentJobExp - g.BeforeJobExp;
+		local e1 = g.CurrentExp + g.TotalExp - g.BeforeExp;
+		local e2 = g.CurrentJobExp + g.TotalJobExp - g.BeforeJobExp;
 		--前回キャラ名と今回キャラ名が違う場合、何も獲得していない場合、表示しない。
 		if g.BeforeCharName == _G.GetMyName() then
 			if g2+e1+e2 ~= 0 then
@@ -78,6 +95,8 @@ function SPEEDRUN_UPDATE(frame, msg, argStr, argNum)
 		g.BeforeMapName = _G.session.GetMapName();
 		g.BeforeExp = g.CurrentExp;
 		g.BeforeJobExp = g.CurrentJobExp;
+		g.TotalExp = 0;
+		g.TotalJobExp = 0;
 	end
 end
 
@@ -97,14 +116,16 @@ function SPEEDRUN_COMMAND()
 	else
 		g3 = "";
 	end
-	local e1 = g.CurrentExp - g.BeforeExp;
-	local e2 = g.CurrentJobExp - g.BeforeJobExp;
+	local e1 = g.CurrentExp + g.TotalExp - g.BeforeExp;
+	local e2 = g.CurrentJobExp + g.TotalJobExp - g.BeforeJobExp;
 	CHAT_SYSTEM("ClearTime:" .. t5 .. "min" .. t4 .. "sec Silver:" .. g3 .. GetCommaedText(g2) .. " Bexp:+" .. GetCommaedText(e1) .. " Jexp:+" .. GetCommaedText(e2));
 	g.OUTPUT(g.BeforeCharName, _G.session.GetMapName() , t5 ,t4,g2,e1,e2);
 	g.BeforeTime = t2;
 	g.BeforeMoney = g1;
 	g.BeforeExp = g.CurrentExp;
 	g.BeforeJobExp = g.CurrentJobExp;
+	g.TotalExp = 0;
+	g.TotalJobExp = 0;
 	--バラックに戻る
 	_G.RUN_GAMEEXIT_TIMER("Barrack");
 end
